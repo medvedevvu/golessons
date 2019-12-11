aa3b4129eecabe1ccfc76fa1ae113d88a1c05973
VASILY MEDVEDEV <medvedevvu72@gmail.com>
2019-12-12 00:01:20 +0300

    Задание 4,5

---
+++ getFirstLastthenpop/getFirstLastthenpop.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, -100}
+	fmt.Println(arr)
+	slast := arr[:]
+	fmt.Println(slast[len(slast)-1])
+	slast = slast[0 : len(slast)-1]
+	fmt.Println(slast)
+	fmt.Println("-------------------")
+	arr = []int{999, 2, 3, 4, 5, 6}
+	slast = arr[:]
+	fmt.Println(slast)
+	fmt.Println(slast[0])
+	copy(slast[0:], slast[1:])
+	slast = slast[0 : len(slast)-1]
+	fmt.Println(slast)
+}
