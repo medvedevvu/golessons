6815c2e87e235885f83b2bf9d93ac764ab8dd16f
VASILY MEDVEDEV <medvedevvu72@gmail.com>
2019-12-11 22:56:18 +0300

    Задание 1,2,3

--- addFirstLast/addFirstLast.go
+++ addFirstLast/addFirstLast.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	myslice := ([]int{7, 7, 7, 7, 7, 7, 7, 7})[:]
 	tmp := make([]int, len(myslice)+1)
-	tmp[0] = 1
+	tmp[0] = 5
 	copy(tmp[1:], myslice) // в переди
 	fmt.Println(tmp)
-	fmt.Println(append(myslice, 1)) // позади
+	fmt.Println(append(myslice, 5)) // позади
 }
