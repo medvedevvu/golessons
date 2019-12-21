27059ec0f1c51d12573c93da8f4d4d026faa0a9d
Vasily <medvedevvu72@gmail.com>
2019-12-21 19:04:24 +0400

    -- 1.2 задание --

--- countshopprice/countshopprice.go
+++ countshopprice/countshopprice.go
@@ -34,10 +34,11 @@ func addItemsPrice(itemList map[int]mu.ItemPrice, item mu.ItemPrice) string {
 
 /* Получить цену заказа по списку товаров - если товара
    нет в справочнике - сообщить об этом пользователю
+   вернуть заказ с посчитанной ценой
 */
-func getOrderCost(itemList map[int]mu.ItemPrice, shopList []string) float32 {
+func getOrderCost(itemList map[int]mu.ItemPrice, shopList mu.Order) float32 {
 	var ordrCost float32 = 0
-	for _, shopName := range shopList { // бегу по списку товаров в заказе
+	for _, shopName := range shopList.Items { // бегу по списку товаров в заказе
 		fond := false
 		for _, item := range itemList { // бегу по списку товаров в каталоге
 			if shopName == item.ItemName {
@@ -53,8 +54,52 @@ func getOrderCost(itemList map[int]mu.ItemPrice, shopList []string) float32 {
 	return ordrCost
 }
 
+/*
+сохраним список товаров с ценой во время запроса от пользователя
+*/
+
+func compStrArr(in1, in2 []string) bool {
+	if len(in1) != len(in2) {
+		return false
+	}
+	rez := false
+	for i := 0; i < len(in1); i++ {
+		rez = (in1[i] == in2[i])
+	}
+	return rez
+}
+
+func seveListwithCost(
+	ordersPrice []mu.Order, // списки товаров с ценами
+	itemsPrice map[int]mu.ItemPrice, // справочник товаров
+	itemsList mu.Order) mu.Order { // список товаров заказа
+	// посмотрим , есть ли такая запись в справочнике список товаров - с ценой
+	if ordersPrice == nil {
+		itemsList.TotalSum = getOrderCost(itemsPrice, itemsList)
+		ordersPrice = append(ordersPrice, itemsList)
+		return itemsList
+	}
+
+	exists := false
+	tmpordersPrice := mu.Order{}
+	for oidx, oItem := range ordersPrice {
+		tmpordersPrice = ordersPrice[oidx]
+		if exists = compStrArr(oItem.Items, itemsList.Items); exists { // такой список товаров уже есть
+			break
+		}
+	}
+	if !exists {
+		itemsList.TotalSum = getOrderCost(itemsPrice, itemsList)
+		ordersPrice = append(ordersPrice, itemsList)
+		return itemsList
+	} else {
+		fmt.Printf("Список %s уже есть \n", tmpordersPrice.Items)
+		return tmpordersPrice
+	}
+}
+
 func main() {
-	itemsPrice := map[int]mu.ItemPrice{}
+	itemsPrice := map[int]mu.ItemPrice{} // каталог товаров
 	// --- положим немного данных в каталог
 	itemsPrice[0] = mu.ItemPrice{ItemName: "Спички", ItemPrice: 1.2}
 	itemsPrice[1] = mu.ItemPrice{ItemName: "Хлеб", ItemPrice: 20.15}
@@ -70,14 +115,35 @@ func main() {
 	fmt.Println(itemsPrice)
 
 	fmt.Println("----- получить цену заказа -----")
-	fmt.Printf("Цена заказа %.2f\n", getOrderCost(itemsPrice, []string{"Хлеб", "Сосиски", "Салями"}))
+	vTempOrder := mu.Order{[]string{"Хлеб", "Сосиски", "Салями"}, 0}
+	fmt.Printf("Цена заказа %.2f\n", getOrderCost(itemsPrice, vTempOrder))
 
-	acountList := map[int]mu.User{}
+	acountList := map[int]mu.User{} // каталог пользователей
 	// --- положим немного данных о пользователях
 	acountList[0] = mu.User{UserName: "Вася", Account: 800.0}
 	acountList[1] = mu.User{UserName: "Коля", Account: 200.0}
 	acountList[2] = mu.User{UserName: "Дима", Account: 300.0}
-	acountList[2] = mu.User{UserName: "Петр", Account: 125.0}
+	acountList[3] = mu.User{UserName: "Петр", Account: 125.0}
 	fmt.Println(acountList)
+
+	// Список счетов - история покупок
+	//         ID accountList --> ID Order --> Сумма заказа
+	billList := map[int]map[int]float32{}
+	billList[0] = map[int]float32{0: 0.0}
+	billList[1] = map[int]float32{0: 0.0}
+	billList[2] = map[int]float32{0: 0.0}
+	billList[3] = map[int]float32{0: 0.0}
+	// первоначально нулевая история
+	fmt.Println(billList)
+
+	fmt.Println("----- 7 -----")
+
+	ordersPrice := []mu.Order{} // список заказов с посчитанной общей ценой
+
+	fmt.Println(seveListwithCost(ordersPrice, itemsPrice, mu.Order{[]string{"Хлеб", "Сосиски"}, 0}))
+	fmt.Println(seveListwithCost(ordersPrice, itemsPrice, mu.Order{[]string{"Хлеб", "Сыр"}, 0}))
+	fmt.Println(seveListwithCost(ordersPrice, itemsPrice, mu.Order{[]string{"Хлеб", "Рыба"}, 0}))
+	fmt.Println(seveListwithCost(ordersPrice, itemsPrice, mu.Order{[]string{"Хлеб", "Рыба"}, 0}))
+	fmt.Println(seveListwithCost(ordersPrice, itemsPrice, mu.Order{[]string{"Хлеб", "Рыба", "Ветчина"}, 0}))
 
 }
--- mutils/structsdef/structsdef.go
+++ mutils/structsdef/structsdef.go
@@ -8,18 +8,12 @@ type ItemPrice struct {
 
 // Order - описание заказа
 type Order struct {
-	Items    map[int]ItemPrice // элементы [кол-во] ---> товар
-	TotalSum float32           // сумма заказа
+	Items    []string // список товаров
+	TotalSum float32  // сумма заказа
 }
 
 // User - описание пользователя
 type User struct {
 	UserName string  // Имя пользователя
 	Account  float32 // остаток на счету
-}
-
-// Bill - описание счетов пользователя
-type Bill struct {
-	UserID    float32         // пользователь
-	DebitHist map[int]float32 // история списаний id заказа ---> сумма покупки
 }
