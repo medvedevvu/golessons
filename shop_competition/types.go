681765ea4bcdd545c12ad39eb95d346cdc655ef7
VASILY MEDVEDEV <medvedevvu72@gmail.com>
2020-01-20 21:52:36 +0400

    -- install blueprint for shop --

---
+++ shop_competition/types.go
@@ -0,0 +1,46 @@
+package shop_competition
+
+const (
+	ProductNormal ProductType = iota
+	ProductPremium
+	ProductSample
+
+	BundleNormal BundleType = iota
+	BundleSample
+
+	AccountNormal AccountType = iota
+	AccountPremium
+
+	SortByName AccountSortType = iota
+	SortByNameReverse
+	SortByBalance
+)
+
+//ProductType one of ProductNormal/ProductPremium/ProductSample
+type ProductType uint8
+type BundleType uint8
+type AccountType uint8
+type AccountSortType uint8
+
+type Product struct {
+	Name  string
+	Price float32
+	Type  ProductType
+}
+
+type Order struct {
+	Products []Product
+	Bundles  []Bundle
+}
+
+type Bundle struct {
+	Products []Product
+	Type     BundleType
+	Discount float32
+}
+
+type Account struct {
+	Name    string
+	Balance float32
+	AccountType
+}
