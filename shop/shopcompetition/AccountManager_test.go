383e2c81a738742edc195792b6d308f64c94efbb
VASILY MEDVEDEV <medvedevvu72@gmail.com>
2020-01-24 00:26:26 +0400

    -- пытаюсь тестить --

--- shop/shopcompetition/AccountManager_test.go
+++ shop/shopcompetition/AccountManager_test.go
@@ -1,3 +1,14 @@
 package shopcompetition
 
-//import "tests"
+import (
+	"testing"
+)
+
+func TetsRegister(t *testing.T) {
+	accList := new(AccountList)
+	err := accList.Register("Vasily")
+	if err != nil {
+		t.Errorf(" не смогли зарегестрировать ")
+	}
+	t.Log(accList)
+}
