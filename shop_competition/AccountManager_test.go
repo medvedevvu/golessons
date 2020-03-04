599344bd9fd0faaf2d9254d1c6aab35f4c63ba94
VASILY MEDVEDEV <medvedevvu72@gmail.com>
2020-03-04 23:24:14 +0400

    -- Balance test 3--

--- shop_competition/AccountManager_test.go
+++ shop_competition/AccountManager_test.go
@@ -107,7 +107,7 @@ func TestRegisterEmptyNameAccountsList(t *testing.T) {
 	wg.Wait()
 }
 func TestAddBalance(t *testing.T) {
-	return nil
+	//return nil
 	vtest := InitAccountList()
 	names := map[string]float32{"Kola": 325.12,
 		"Vasiy": 900.21, "Dram": 10, "Vortis": 23}
