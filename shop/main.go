cfe7cf0b7487cafa2a7ced864a8a38e290c8d814
VASILY MEDVEDEV <medvedevvu72@gmail.com>
2020-03-18 22:41:55 +0400

    -- add TestManyGorutins --

--- shop/main.go
+++ shop/main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-	sc "shop_competition"
 )
 
 func main() {
-	acc := sc.NewAccountsList()
-	err := acc.Register("Lova")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(acc)
+	fmt.Println("test")
 }
---
+++ shop_competition/ManyGorutins_test.go
@@ -0,0 +1,55 @@
+package shop_competition
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestManyGorutins(t *testing.T) {
+	groutList := []string{"TestAddProduct", 
+						   "TestRemoveProduct", 
+						   "TestOptModifyProducts",
+						   "TestAddAndRemoveBoundle",
+						   "TestSimpleRemoveBoundle" ,
+						   "TestAddBalance" ,
+						   "TestCircleCheckBalance",
+						   "TestAsyncPlaceOrder" ,
+						   "TestPlaceOrderAndAddBalanc"}
+	groutCount := 50
+	var wg sync.WaitGroup
+	for idx := 1; idx <= groutCount; idx++ {
+		value := rand.Int31n(int32(len(groutList)))
+		wg.Add(1)
+		go func(value int32, t  *testing.T) {
+			defer wg.Done()
+			TaskGo(value,t)
+			time.Sleep(time.Microsecond)
+		}(value, t )
+	}
+	wg.Wait()
+	fmt.Println("Ok")
+}
+
+func TaskGo(id int32, t *testing.T) {
+	switch {
+	case id == 0:
+		TestAddProduct(t)
+	case id == 1:
+		TestRemoveProduct(t)
+	case id == 2:
+		TestOptModifyProducts(t)
+	case id == 3:
+		TestAddAndRemoveBoundle(t)
+	case id == 4:
+		TestSimpleRemoveBoundle(t)
+	case id == 5:	
+		TestAddBalance(t)
+	case id == 6:	
+		TestAsyncPlaceOrder(t)
+	case id == 7:	
+		TestPlaceOrderAndAddBalanc(t)
+	}
+}
